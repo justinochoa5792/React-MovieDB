97abfc10472f66eed0449480acd5cbbc62a08d0c
Justin Ochoa <just.och92@gmail.com>
2021-10-17 11:54:35 -0400

    fixed missing rating

--- src/component/Popup.jsx
+++ src/component/Popup.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Popup = ({ selected, closePopUp }) => {
+  console.log(selected);
   return (
     <section className="popup">
       <div className="content">
         <h2>
           {selected.Title} <span>({selected.Year})</span>
         </h2>
-        <p className="rating">Rating: {selected.Rating}</p>
+        <p className="rating">Rating: {selected.imdbRating}</p>
         <div className="plot">
           <img src={selected.Poster} alt={selected.Title} />
           <p>{selected.Plot}</p>
